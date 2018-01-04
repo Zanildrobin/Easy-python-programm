765be926432657514a3ae9d63b0101f822a0546c
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2018-01-04 16:06:32 +0100

    Refactor file reading

--- land_hauptstadt_zuweiser.py
+++ land_hauptstadt_zuweiser.py
@@ -1,16 +1,21 @@
 laender = {}
 laender_2 = {}
-fobj = open("hauptstaedte.txt", "r")
-for line in fobj:
-    line = line.strip()
+
+with open("hauptstaedte.txt", "r") as fobj:
+    lines = fobj.readlines()
+    lines = [line.strip() for line in lines]
+
+for line in lines:
     zuordnung = line.split(" ")
     laender[zuordnung[0]] = zuordnung[1]
     laender_2[zuordnung[1]] = zuordnung[0]
-fobj.close()
+
 while True:
-    inp = input("Geben Sie eine Stadt oder ein Land ein, um die zugehörige Hauptstadt/das zugehörige Land zu erhalten.")
+    print("Geben Sie eine Stadt oder ein Land ein, um die zugehörige Hauptstadt/das zugehörige Land zu erhalten.")
+    inp = input()
+
     if inp in laender:
-        print("Die zugehörige Hauptstadt ist:",laender[inp])
+        print("Die zugehörige Hauptstadt ist:", laender[inp])
     elif inp in laender_2:
         print("Das zugehörige Land ist:", laender_2[inp])
     else:
